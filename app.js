69e2680ecae3c922eb022139eb81f3b8c371677d
philippepowell24 <45657187+philippepowell24@users.noreply.github.com>
2020-01-20 10:31:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -79,6 +79,7 @@ function init() {
     scores = [0 , 0];
     activePlayer = 0;
     roundScore = 0;
+    gamePlaying = true;
 
     document.querySelector('.dice').style.display = 'none';
     document.getElementById('score-0').textContent = '0';
